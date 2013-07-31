bb3c59fca72dc954b01de4db38df90e318297bcc
Tom McLaughlin <tmclaughlin@hubspot.com>
2013-07-30 21:06:15 -0400

    Break out package into its own class.

---
+++ manifests/package.pp
@@ -0,0 +1,27 @@
+# Class: rabbitmq::service
+#
+#   This class manages the rabbitmq package itself.
+#
+#   Jeff McCune <jeff@puppetlabs.com>
+#   Tom McLaughlin <tmclaughlin@hubspot.com>
+#
+# Parameters:
+#  [*package_name*]- name of rabbitmq package
+#  [*pkg_ensure*] - ensure value of the rabbitmq package resource
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class rabbitmq::package (
+  $package_name,
+  $pkg_ensure
+) {
+
+  package { $package_name:
+    ensure => $pkg_ensure,
+  }
+
+}
--- manifests/server.pp
+++ manifests/server.pp
@@ -17,7 +17,7 @@
 #  [*cluster_disk_nodes*] - which nodes to cluster with (including the current one)
 #  [*erlang_cookie*] - erlang cookie, must be the same for all nodes in a cluster
 #  [*wipe_db_on_cookie_change*] - whether to wipe the RabbitMQ data if the specified
-#    erlang_cookie differs from the current one. This is a sad parameter: actually, 
+#    erlang_cookie differs from the current one. This is a sad parameter: actually,
 #    if the cookie indeed differs, then wiping the database is the *only* thing you
 #    can do. You're only required to set this parameter to true as a sign that you
 #    realise this.
@@ -25,7 +25,7 @@
 #  stdlib
 # Sample Usage:
 #
-#  
+#
 #
 #
 # [Remember: No empty lines between comments and class definition]
@@ -71,10 +71,12 @@ class rabbitmq::server(
 
   $plugin_dir = "/usr/lib/rabbitmq/lib/rabbitmq_server-${version_real}/plugins"
 
-  package { $package_name:
-    ensure => $pkg_ensure_real,
-    notify => Class['rabbitmq::service'],
+  class { 'rabbitmq::package':
+    package_name => $package_name,
+    pkg_ensure   => $pkg_ensure_real,
+    notify       => Class['rabbitmq::service'],
   }
+
 
   file { '/etc/rabbitmq':
     ensure  => directory,
